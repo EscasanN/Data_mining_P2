9211791ba7a84a5845c11764b59abd4da7c5e7f4
Andre <Andrewlol774@gmail.com>
2025-02-27 21:36:36 -0600

    Adding hopkins to see if I can group them, turns out it can be

---
+++ avances2.R
@@ -0,0 +1,50 @@
+
+# Se junta estos dos datos, el id con el precio de la vivienda con el resto de datos para poder
+#hacer un analisis extenso ante los datos de entrenamiento y prueba.
+library(cluster) #Para calcular la silueta
+library(e1071)#para cmeans
+library(mclust) #mixtures of gaussians
+library(fpc) #para hacer el plotcluster
+library(NbClust) #Para determinar el número de clusters óptimo
+library(factoextra) #Para hacer gráficos bonitos de clustering
+library(hopkins) #Para revisar si vale la pena hacer agrupamiento
+library(GGally) #Para hacer el conjunto de graficos
+library(FeatureImpCluster) #Para revisar la importancia de las variables en los grupos.
+library(pheatmap) #Para hacer mapa de calor
+
+
+
+data <- read.csv("test.csv")
+datasample <- read.csv("sample_submission.csv")
+datatrain <- read.csv("train.csv")
+
+data_analysis <- merge(data, datasample, by="Id")
+
+data_analysis <- rbind(data_analysis, datatrain)
+
+View(data_analysis)
+
+
+#Hacer agrupamiento con datos numericos
+data_analysis <- data_analysis[,  c("MSSubClass", "LotFrontage", "LotArea",  "OverallQual", "OverallCond",
+                 "YearBuilt", "YearRemodAdd", "MasVnrArea", "BsmtFinSF1", "BsmtFinSF2", 
+                 "BsmtUnfSF", "TotalBsmtSF", "X1stFlrSF", "X2ndFlrSF", "LowQualFinSF", 
+                 "GrLivArea", "BsmtFullBath", "BsmtHalfBath", "FullBath", "HalfBath" ,
+                 "BedroomAbvGr", "KitchenAbvGr", "TotRmsAbvGrd", "Fireplaces", "GarageYrBlt"
+                 , "GarageCars", "GarageArea", "WoodDeckSF" , "OpenPorchSF", "EnclosedPorch"
+                 , "X3SsnPorch", "ScreenPorch", "PoolArea", "MiscVal", "MoSold", "YrSold",
+                 "SalePrice")]
+
+#escalado de datos para ajustarlo a rangos especificos
+data_analysis <- scale(data_analysis)
+
+
+#Realizar hopkins para ver si podemos agruparlos o no
+hopkins(data_analysis)
+#El valor del estadístico de hopkins está alejado de 0.5 por lo que los datos no son aleatorios hay altas posibilidades de que sea factible el agrupamiento.  
+#Ademas vemos que alcanza a 1 lo cual significa que si hay tendencia fuerte de agrupamientos
+
+#1``
+
+#CLUSTERING Y PCA
+
