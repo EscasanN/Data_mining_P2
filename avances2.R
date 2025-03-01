2c4169c7f2a4e68ea22ad5349a897f648e72d4ed
Andre <Andrewlol774@gmail.com>
2025-02-28 23:35:45 -0600

    finished with html in R

--- avances2.R
+++ avances2.R
@@ -22,8 +22,6 @@ data_analysis <- merge(data, datasample, by="Id")
 
 data_analysis <- rbind(data_analysis, datatrain)
 
-View(data_analysis)
-
 
 #Hacer agrupamiento con datos numericos
 data_analysis <- data_analysis[,  c("MSSubClass", "LotFrontage", "LotArea",  "OverallQual", "OverallCond",
@@ -35,16 +33,43 @@ data_analysis <- data_analysis[,  c("MSSubClass", "LotFrontage", "LotArea",  "Ov
                  , "X3SsnPorch", "ScreenPorch", "PoolArea", "MiscVal", "MoSold", "YrSold",
                  "SalePrice")]
 
+na.omit(data_analysis)
+
 #escalado de datos para ajustarlo a rangos especificos
 data_analysis <- scale(data_analysis)
+data_hop <- scale(data_analysis)
 
 
 #Realizar hopkins para ver si podemos agruparlos o no
-hopkins(data_analysis)
+hopkins(data_hop)
 #El valor del estadístico de hopkins está alejado de 0.5 por lo que los datos no son aleatorios hay altas posibilidades de que sea factible el agrupamiento.  
 #Ademas vemos que alcanza a 1 lo cual significa que si hay tendencia fuerte de agrupamientos
 
-#1``
+#1`
+datos_dist<- dist(data_hop)
+fviz_dist(datos_dist, show_labels = F)
+
+
+
+
+wss=0
+for (i in 1:10) 
+  wss[i] <- sum(kmeans(na.omit(data_analysis), centers=i)$withinss)
+
+plot(1:10, wss, type="b", xlab="Number of Clusters",  ylab="Within groups sum of squares")
+
+
+fviz_nbclust(na.omit(data_analysis), kmeans, method = "wss") +
+  labs(subtitle = "Elbow method")
+
+
+#en este caso podemos observar que el numero de clusters a elegir es 3 como se obseva en la grafica ya esta empezando a reducir su decenso a llegar a uno estable
+
+
+km<-kmeans(data_analysis,3,iter.max =100)
+
+data$grupo<-km$cluster
+
 
 #CLUSTERING Y PCA
 
